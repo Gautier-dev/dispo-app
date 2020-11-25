9fb2e22e7f3a2616c242973efdc5c4865bd2af6e
tletutour <tom.le-tutour@insa-lyon.com>
2020-11-25 16:42:46 +0100

    added angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router'
 import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
 import { ProgressBar } from './progressBar.component';
-
+const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +16,8 @@ import { ProgressBar } from './progressBar.component';
   ],
   imports: [
     BrowserModule,
-    ClarityModule
+    ClarityModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
